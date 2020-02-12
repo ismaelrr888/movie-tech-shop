1c8830167bc9a7516bd9c416b738745cefdd6372
ismael <ismaelrr888@gmail.com>
2020-02-12 10:38:48 -0500

    fix redux dev tools in browser not install redux dev tools

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -11,7 +11,10 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : compose
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
